837a5dcb9331940dab70ffeb12ddd0df2703f538
Jandro <jov20824@educastur.es>
2022-12-21 10:11:56 +0100

    R.E.N.O. 1

---
+++ R.E.N.O._Alejandro_Blanco/src/main/Start.java
@@ -0,0 +1,19 @@
+package main;
+
+import clases.Robot;
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+
+		Robot marcos = new Robot ("Marcos");
+		
+		Robot juanjo = new Robot ("Juanjo");
+		
+		System.out.println(marcos);
+		System.out.println(juanjo);
+		
+	}
+
+}
